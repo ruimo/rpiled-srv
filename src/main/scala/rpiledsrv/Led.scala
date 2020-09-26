719aefbe540c860c206f4893647ea7321d7063f7
Shisei Hanai <ruimo.uno@gmail.com>
2020-09-26 17:16:03 +0900

    Init LED.

--- src/main/scala/rpiledsrv/Led.scala
+++ src/main/scala/rpiledsrv/Led.scala
@@ -5,6 +5,12 @@ import com.pi4j.io.gpio.{GpioPinDigitalOutput, PinState}
 import scala.concurrent.duration.Duration
 
 class Led(name: String, pin: GpioPinDigitalOutput, isNegativeLogic: Boolean = false) {
+  if (isNegativeLogic) {
+    pin.high()
+  } else {
+    pin.low()
+  }
+
   def blink(dur: Duration) {
     pin.pulse(dur.toMillis, if (isNegativeLogic) PinState.LOW else PinState.HIGH)
   }

6797d1d612db08f06ebd1804df12481288da1d8e
Shisei Hanai <ruimo.uno@gmail.com>
2020-12-18 09:01:58 +0900

    Change IpTable logic.

--- src/main/scala/rpiledsrv/IpTable.scala
+++ src/main/scala/rpiledsrv/IpTable.scala
@@ -1,17 +1,73 @@
 package rpiledsrv
 
 import scala.collection.{mutable => mut}
+import scala.annotation.tailrec
 
 class IpTable {
+  sealed trait IpSlot
+  object IpSlot {
+    case object Vacant extends IpSlot
+    case class Element(ip: String, lastUpdated: Long) extends IpSlot
+  }
+
   val MaxEntries = 2
-  private[this] var ipTable = new mut.LinkedHashSet[String]
+  private[this] var ipTable: mut.Seq[IpSlot] = mut.Seq.fill(MaxEntries)(IpSlot.Vacant)
 
   def registerIpAndGetIndex(ip: String): Int = {
-    ipTable.add(ip)
-    if (MaxEntries < ipTable.size) {
-      ipTable = ipTable.drop(1)
+    @tailrec def findExisting(idx: Int): Int = {
+      if (ipTable.length <= idx) -1
+      else {
+        ipTable(idx) match {
+          case IpSlot.Vacant =>
+            findExisting(idx + 1)
+          case IpSlot.Element(eip, lastUpdated) =>
+            if (eip == ip) {
+              idx
+            } else {
+              findExisting(idx + 1)
+            }
+        }
+      }
     }
-    ipTable.toVector.indexOf(ip)
+
+    val idx = findExisting(0)
+    if (idx != -1) {
+      ipTable(idx) = IpSlot.Element(ip, System.currentTimeMillis)
+      idx
+    } else {
+      val vacantIdx = ipTable.indexWhere(_ == IpSlot.Vacant)
+      if (vacantIdx != -1) {
+        ipTable(vacantIdx) = IpSlot.Element(ip, System.currentTimeMillis)
+        vacantIdx
+      } else {
+        @tailrec def findOldest(curIdx: Int, oldestIdx: Int = -1): Int = {
+          if (ipTable.length <= curIdx) oldestIdx
+          else {
+            ipTable(curIdx) match {
+              case IpSlot.Vacant => findOldest(curIdx + 1, oldestIdx)
+              case IpSlot.Element(eip, lastUpdated) =>
+                if (oldestIdx == -1)
+                  findOldest(curIdx + 1, curIdx)
+                else {
+                  if (lastUpdated < ipTable(oldestIdx).asInstanceOf[IpSlot.Element].lastUpdated)
+                    findOldest(curIdx + 1, curIdx)
+                  else
+                    findOldest(curIdx, oldestIdx)
+                }
+            }
+          }
+        }
+
+        val oldestIdx = findOldest(0)
+        ipTable(oldestIdx) = IpSlot.Element(ip, System.currentTimeMillis)
+        oldestIdx
+      }
+    }
+  }
+
+  def indexOf(ip: String): Int = ipTable.indexWhere {
+    case IpSlot.Vacant => false
+    case IpSlot.Element(eip, _) => eip == ip
   }
 }
 
--- src/test/scala/rpiledsrv/IpTableSpec.scala
+++ src/test/scala/rpiledsrv/IpTableSpec.scala
@@ -4,19 +4,33 @@ import org.specs2.mutable._
 
 class IpTableSpec extends Specification {
   "IpTable" should {
-    "Can register ip" in {
+    "The last accessed IP should retain pattern 0" in {
       val tbl = new IpTable()
 
       tbl.registerIpAndGetIndex("9.8.7.6") === 0
       tbl.registerIpAndGetIndex("0.1.2.3") === 1
+      tbl.registerIpAndGetIndex("9.8.7.6") === 0 // Last accessed IP.
+
+      // It should maintain latest two IPs. Oldest "0.1.2.3" should be deleted.
+      tbl.registerIpAndGetIndex("5.5.5.5") === 1
+      tbl.indexOf("0.1.2.3") === -1
+      tbl.indexOf("9.8.7.6") === 0
+      tbl.indexOf("5.5.5.5") === 1
+    }
+
+    "The last accessed IP should retain pattern 1" in {
+      val tbl = new IpTable()
 
       tbl.registerIpAndGetIndex("9.8.7.6") === 0
       tbl.registerIpAndGetIndex("0.1.2.3") === 1
+      tbl.registerIpAndGetIndex("9.8.7.6") === 0
+      tbl.registerIpAndGetIndex("0.1.2.3") === 1 // Last accessed IP.
 
       // It should maintain latest two IPs. Oldest "9.8.7.6" should be deleted.
-      tbl.registerIpAndGetIndex("5.5.5.5") === 1
-      tbl.registerIpAndGetIndex("0.1.2.3") === 0
-      tbl.registerIpAndGetIndex("5.5.5.5") == 1
+      tbl.registerIpAndGetIndex("5.5.5.5") === 0
+      tbl.indexOf("9.8.7.6") === -1
+      tbl.indexOf("0.1.2.3") === 1
+      tbl.indexOf("5.5.5.5") === 0
     }
   }
 }
